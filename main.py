7659df8135d29a74f7621d338371afc5af7e238d
elad-weiss <elad_Weiss@hotmail.com>
2022-07-17 13:12:53 +0300

    added document title settings

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ print("Enter file directory")
 dir_ = input()
 if dir_ == "t":
     dir_ = "content.txt"
+
 with open(dir_, "r") as f:
     content = f.readlines()
 
@@ -14,31 +15,40 @@ with open("shell.html", "r") as shell:
     head = doc.head
 
 # divides the text into paragraphs
-paragraphs = []
-paragraph = []
+paragraphs = [[]]
+offset = 0
+
 for i in range(len(content)):
     if content[i] == "\n":
-        paragraphs.append(paragraph)
-        paragraph.clear()
+        paragraphs.append([])
+        offset += 1
     else:
-        paragraph.append(content[i])
-if content[len(content) - 1] != "\n":
-    paragraphs.append(paragraph)
+        paragraphs[offset].append(content[i])
+
+# add a title for the body of the document
+title_suggestion = paragraphs[0][0]
+
+# indicates whether or not the title is in the contents(if so it does not write the title as a paragraph): 0 = False, 1 = True
+title_included = 0
+
+print(
+    f'Enter a title for your document(enter "@f" to make "{title_suggestion}" the title): ')
+doc_title = input()
+if doc_title == "@f":
+    doc_title = title_suggestion
+    title_included = 1
+title = body.h1
+title.string = doc_title
 
 # add the paragraph to the website
-for par in paragraphs:
+for par in range(title_included, len(paragraphs)):
     new_paragraph = doc.new_tag("p")
     body.append(new_paragraph)
 
-    for line in par:
+    for line in paragraphs[par]:
         new_paragraph.append(line)
         new_row = doc.new_tag("br")
         new_paragraph.append(new_row)
-
-print("Enter a title for your document: ")
-doc_title = input()
-title = body.h1
-title.string = doc_title
 
 print("Enter a title for your website: ")
 site_title = input()
